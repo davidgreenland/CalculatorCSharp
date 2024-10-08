29b806cb6dfe847ff1e6f4d17c926029fa189e51
David Greenland <David.Greenland@brighthr.com>
2024-10-08 10:51:46 +0100

    YLW-3176: Added failing test for new inputs.

--- Tests/CalculatorTests.cs
+++ Tests/CalculatorTests.cs
@@ -6,13 +6,23 @@ namespace Tests
     public class CalculatorTests
     {
         [Test]
-        public void Add_returns_zero_when_both_inputs_are_zero() 
+        public void Add_returns_zero_when_both_inputs_are_zero()
         {
             var calculator = new Calculator();
 
             var result = calculator.Add(0, 0);
 
             Assert.That(result, Is.EqualTo(0), "Calculator did not return zero");
+        }
+
+        [Test]
+        public void Add_returns_three_when_inputs_are_one_and_two()
+        {
+            var calculator = new Calculator();
+
+            var result = calculator.Add(1, 2);
+
+            Assert.That(result, Is.EqualTo(3), "Basic addition did not return 3");
         }
     }
 }
