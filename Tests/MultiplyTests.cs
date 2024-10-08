5ad20455e16cf081f11e37e8c130732ce17ece15
David Greenland <David.Greenland@brighthr.com>
2024-10-08 13:17:23 +0100

    YLW-3176: Added failing test for Multiply method. Renames to SubtractTests.

---
+++ Tests/MultiplyTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using CalculatorProject;
+
+namespace Tests
+{
+    public class MultiplyTests
+    {
+        private Calculator _calculator;
+
+        [SetUp]
+        public void Setup() => _calculator = new Calculator();
+
+        [Test]
+        public void Multiply_returns_one_when_inputs_are_both_one()
+        {
+            var result = _calculator.Multiply(1, 1);
+
+            Assert.That(result, Is.EqualTo(1), $"Basic multiplication did not return one");
+        }
+    }
+}
