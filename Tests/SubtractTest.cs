0343bbd034e96b6e39bd2a991d9156e656186989
David Greenland <David.Greenland@brighthr.com>
2024-10-08 11:56:20 +0100

    YLW-3176 Added failing test for Subtract.

---
+++ Tests/SubtractTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using CalculatorProject;
+
+namespace Tests
+{
+    public class SubtractTests
+    {
+        [Test]
+        public void Subtract_returns_zero_when_inputs_are_one_and_zero()
+        {
+            var calculator = new Calculator();
+
+            var result = calculator.Subtract(1, 0);
+
+            Assert.That(result, Is.EqualTo(0));
+        }
+    }
+}
